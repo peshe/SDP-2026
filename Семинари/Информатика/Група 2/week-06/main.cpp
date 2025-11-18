9d00b75668e92c0fd13da7b4057dec9897c541f4
Stanimir <sspetrov1@uni-sofia.bg>
2025-11-18 11:12:58 +0200

    fix formatting

--- Семинари/Информатика/Група 2/week-06/main.cpp
+++ Семинари/Информатика/Група 2/week-06/main.cpp
@@ -4,21 +4,21 @@
 
 int main()
 {
-    forward_list<int> list{5,2,4,1};
-    list.insert_after(list.before_begin(), 10);
-    list.insert_after(list.before_begin(), 11);
+	forward_list<int> list{5,2,4,1};
+	list.insert_after(list.before_begin(), 10);
+	list.insert_after(list.before_begin(), 11);
 
-    for(auto it = list.begin(); it != list.end(); ++it)
-        std::cout << *it << ' ';
-    std::cout << '\n';
+	for(auto it = list.begin(); it != list.end(); ++it)
+		std::cout << *it << ' ';
+	std::cout << '\n';
 
-    // 10, 11, 5, 2, 4, 1
-    auto it = list.begin();
-    ++it; ++it; ++it;
-    list.erase_after(it);
-    list.insert_after(it, 100);
+	// 10, 11, 5, 2, 4, 1
+	auto it = list.begin();
+	++it; ++it; ++it;
+	list.erase_after(it);
+	list.insert_after(it, 100);
 
-    for(int x: list)
-        std::cout << x << ' ';
-    std::cout << std::endl;
+	for(int x : list)
+		std::cout << x << ' ';
+	std::cout << std::endl;
 }
