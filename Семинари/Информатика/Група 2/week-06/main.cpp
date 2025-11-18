b89a1e5777955273aae6fd1156131dcfdc59003d
Stanimir <sspetrov1@uni-sofia.bg>
2025-11-18 11:08:52 +0200

    add week-06

---
+++ Семинари/Информатика/Група 2/week-06/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "forward_list.hpp"
+
+int main()
+{
+    forward_list<int> list{5,2,4,1};
+    list.insert_after(list.before_begin(), 10);
+    list.insert_after(list.before_begin(), 11);
+
+    for(auto it = list.begin(); it != list.end(); ++it)
+        std::cout << *it << ' ';
+    std::cout << '\n';
+
+    // 10, 11, 5, 2, 4, 1
+    auto it = list.begin();
+    ++it; ++it; ++it;
+    list.erase_after(it);
+    list.insert_after(it, 100);
+
+    for(int x: list)
+        std::cout << x << ' ';
+    std::cout << std::endl;
+}
